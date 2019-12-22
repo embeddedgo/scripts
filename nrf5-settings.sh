cbfa27a8460899f3df97d63b57c8d9847a7aa024
Michał Derkacz <michal@Lnet.pl>
2019-12-22 01:23:17 +0100

    nrf5-settings.sh gets family from GOTARGET

--- nrf5-settings.sh
+++ nrf5-settings.sh
@@ -4,4 +4,8 @@ set -e
 
 name=$(basename $(pwd))
 
+if [ -z "$family" ]; then
+	family=$(echo $GOTARGET |tr '[a-z]' '[A-Z]')
+fi
+
 nrfutil settings generate --family $family --application $name.hex --application-version $appversion --bootloader-version $bootversion --bl-settings-version 1 $name-settings.hex >/dev/null

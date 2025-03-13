c4653d31283f798de5ed9dcc2e45f107908272b4
Michał Derkacz <michal@Lnet.pl>
2025-03-13 19:56:39 +0100

    build module examples

--- rebuild-examples.sh
+++ rebuild-examples.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for d in ../*/devboard/*/examples/*; do
+for d in ../*/devboard/*/examples/* ../*/devboard/*/module/*/examples/*; do
 	if [ -d $d ]; then
 		echo $d
 		cd $d
@@ -9,7 +9,7 @@ for d in ../*/devboard/*/examples/*; do
 				../build-blank.sh
 			elif [ -x ../build.sh ]; then
 				../build.sh
-			elif [ -f ../build.cfg ]; then
+			else
 				emgo build
 			fi
 		fi
